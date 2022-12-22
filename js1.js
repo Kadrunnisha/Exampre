0700e2fd3fc762cbd5bd345b6187d4d31e537733
Kadrunnisha <106872353+Kadrunnisha@users.noreply.github.com>
2022-12-22 21:55:57 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,21 @@
+const toggleButton=document.getElementsByClassName('toggle-button')[0]
+const navbarlink=document.getElementsByClassName(' l')[0];
+toggleButton.addEventListener('click',() => {
+      navbarlink.classList.toggle('active')
+})
+const navbar=document.getElementsByClassName(' l')[1];
+toggleButton.addEventListener('click',() => {
+    navbar.classList.toggle('active')
+})
+const navba=document.getElementsByClassName(' l')[2];
+toggleButton.addEventListener('click',() => {
+    navba.classList.toggle('active')
+})
+const navb=document.getElementsByClassName(' l')[3];
+toggleButton.addEventListener('click',() => {
+    navb.classList.toggle('active')
+})
+const nav=document.getElementsByClassName(' l')[4];
+toggleButton.addEventListener('click',() => {
+    nav.classList.toggle('active')
+})
